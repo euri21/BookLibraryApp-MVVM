9d98aec3cf50bd95a7e96655ac96f878cfb320ed
solution888 <suko.m@outlook.com>
2022-06-04 19:25:47 +0900

    Add Utilities

---
+++ BookLibraryMVVM/Inject/DependencyInjector.swift
@@ -0,0 +1,8 @@
+//
+//  DependencyInjector.swift
+//  BookLibraryMVVM
+//
+//  Created by Solution888 on 6/4/22.
+//
+
+import Foundation
---
+++ BookLibraryMVVM/System/AppEnvironment.swift
@@ -0,0 +1,14 @@
+//
+//  AppEnvironment.swift
+//  BookLibraryMVVM
+//
+//  Created by Solution888 on 6/4/22.
+//
+
+import Foundation
+
+struct AppEnvironment {
+    
+}
+
+
