7e5f0ec63798311d15b5ad4c0d3aa1ab20c697cf
solution888 <suko.m@outlook.com>
2022-06-04 18:57:51 +0900

    Create initial project

---
+++ BookLibraryMVVM/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  BookLibraryMVVM
+//
+//  Created by q on 6/4/22.
+//
+
+import Foundation
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+}
---
+++ BookLibraryMVVM/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  BookLibraryMVVM
+//
+//  Created by q on 6/4/22.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: ContentView())
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
